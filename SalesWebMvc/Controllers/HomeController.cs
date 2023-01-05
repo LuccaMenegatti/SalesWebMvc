5ebee2f5deecdb53bbb7524cec2e4bf8586bdbbe
Lucca Antonio Menegatti <menegatti.luccaantonio@gmail.com>
2023-01-05 17:29:10 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
+            ViewData["email"] = "menegatti.luccaantonio@gmail.com";
+            ViewData["Developer"] = "Lucca Menegatti";
 
             return View();
         }
